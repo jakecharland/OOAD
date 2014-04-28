d9c25c75b56225c6df13c82260cb3d345e3d1a60
megu5202 <melissa.guba@colorado.edu>
2014-04-27 18:37:38 -0600

    added viewController files

---
+++ BAC Calc/TabBarController.h
@@ -0,0 +1,21 @@
+//
+//  TabBarController.h
+//  BAC Calc
+//
+//  Created by Melissa on 4/27/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UIViewController
+
+// bac tab button
+// profile tab button
+// viewDrinks tab button
+// extras button
+
+// button functions
+// goToView(View)
+
+@end
